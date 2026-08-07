2cdfe52157abb49020751b3412df6389b770fae9
Yue021130 <169446203@qq.com>
2026-08-07 19:17:48 +0800

    feat:添加线程池深度实践启动类

---
+++ 11-thread-pool-advanced-practice/src/main/java/com/example/threadpooladvanced/ThreadPoolAdvancedPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.threadpooladvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadPoolAdvancedPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadPoolAdvancedPracticeApplication.class, args);
+    }
+}
